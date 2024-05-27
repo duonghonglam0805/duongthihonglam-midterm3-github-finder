3c3406362429cefeac3b119c6737ea39257ed552
duonghonglam0805 <honglamhk2004@gmail.comZ>
2024-05-28 06:43:05 +0700

    Refactor: Create Home component

--- src/App.js
+++ src/App.js
@@ -2,42 +2,14 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
-import About from "./components/About";
 import Navbar from "./components/layout/NavBar";
-import User from "./components/users/User";
-import Search from "./components/users/Search";
-import NotFound from "./components/NotFound";
-import { fetchDataApi } from "./data/api";
+import Home from "./components/layout/Home";
 function App() {
-  const [users, setUsers] = useState([]);
-    const fetchData = async () => {
-      try {
-        const users = await fetchDataApi();
-        setUsers(users);
-        console.log("GitHub Users:", users);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    
-useEffect(() => {
-  fetchData()
-},[]);
-
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <div className="container">
-          <h1>GitHub Users Data</h1>
-          <Switch>
-            <Route exact path= "/" component={Search} />
-            <Route exact path= "/about" component={About} />
-            {/* Add this line to specify the routing */}
-            <Route exact path="/user/:id" component={User} />
-            <Route exact path= "/*" component={NotFound} />
-          </Switch>
-        </div>
+        <Home />
       </Router>
     </div>
   );
---
+++ src/components/layout/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Switch} from 'react-router-dom/cjs/react-router-dom.min';
+import User from '../users/User';
+import Search from '../users/Search';
+import About from '../pages/About';
+import NotFound from '../pages/NotFound';
+const Home = () => {
+  return (
+    <div className="container">
+    <h1>GitHub Users Data</h1>
+    <Switch>
+      <Route exact path= "/" component={Search} />
+      <Route exact path= "/about" component={About} />
+      {/* Add this line to specify the routing */}
+      <Route exact path="/user/:id" component={User} />
+      <Route exact path= "/*" component={NotFound} />
+    </Switch>
+  </div>
+  )
+}
+
+export default Home
