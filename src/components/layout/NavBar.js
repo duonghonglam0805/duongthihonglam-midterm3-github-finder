dfeeae5beed0bc727a24e794ef63b35cad2a8020
duonghonglam0805 <honglamhk2004@gmail.comZ>
2024-05-28 15:31:15 +0700

    Implement Dark theme / Light theme mechanic

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { CiLight } from "react-icons/ci";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.body.className = theme === "light" ? "bg-light " : "bg-dark";
+  }, [theme]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
   return (
     <nav className="navbar bg-success">
       <h1>
@@ -8,6 +20,7 @@ const Navbar = () => {
       </h1>
       <ul>
         <li>
+          <CiLight onClick={handleClick} />
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
         </li>
